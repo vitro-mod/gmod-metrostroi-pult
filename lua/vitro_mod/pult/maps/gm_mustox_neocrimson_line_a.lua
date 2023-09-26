2aa2c490ec136d117d30e1a8740c9fbab08e4e67
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2023-09-27 00:26:11 +0400

    swtRemap table

--- lua/vitro_mod/pult/maps/gm_mustox_neocrimson_line_a.lua
+++ lua/vitro_mod/pult/maps/gm_mustox_neocrimson_line_a.lua
@@ -1,5 +1,14 @@
 VitroMod.Pult.Map = {
     Init = function()
+        local swtRemap = {
+			{gm = 'AutoInstance4trackswitch_551_1', pult = 'br1'},
+
+		}
+		for _,r in pairs(swtRemap) do
+			for k,v in pairs(ents.FindByName(r.gm)) do
+				v:SetName('trackswitch_' .. r.pult)
+			end
+		end
 		VitroMod.Pult.SwitchesInvert = {}
     end,
     
