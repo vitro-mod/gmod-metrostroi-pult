eca1135e41b256f209a1f7b4ec0a3825eb41e374
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2023-11-09 02:31:58 +0400

    rename singals and signs entity to override minsk

--- lua/metrostroi/sh_minsk_custom_signal.lua
+++ lua/metrostroi/sh_minsk_custom_signal.lua
@@ -1,4 +1,4 @@
 timer.Simple(1.1, function()  
-    scripted_ents.Alias ( "gmod_track_signal", "gmod_track_signal_fixed")
-    scripted_ents.Alias ( "gmod_track_signs", "gmod_track_signs_fixed")
+    scripted_ents.Alias ( "gmod_track_signal", "gmod_track_signal_new" )
+    scripted_ents.Alias ( "gmod_track_signs", "gmod_track_signs_new" )
 end)
