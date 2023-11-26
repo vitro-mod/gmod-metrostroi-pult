3ca1a254e03e2b4579b096535a433c0d69dced6f
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2023-11-26 20:58:03 +0400

    Update config.example.lua

--- lua/vitro_mod/pult/config.example.lua
+++ lua/vitro_mod/pult/config.example.lua
@@ -4,6 +4,7 @@ VitroMod.Pult.Urls = {
 	["gm_metro_kalinin_v3"] = 'ws://localhost:8092/',
 	["gm_metro_minsk_1984"] = 'ws://localhost:8090/',
 	["gm_metro_nekrasovskaya_line_v6"] = 'ws://localhost:8081/',
+	["gm_mustox_neocrimson_line_a"] = 'ws://localhost:8081/',
 }
 VitroMod.Pult.Name = 'MASTER'
 VitroMod.Pult.Key = 'gm'
