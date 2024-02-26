97ca23ad6d3ac9cbd13abcf200471fb207169328
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-02-26 20:33:39 +0400

    example for kalina v3

--- lua/vitro_mod/pult/config.example.lua
+++ lua/vitro_mod/pult/config.example.lua
@@ -1,7 +1,7 @@
 VitroMod = VitroMod or {}
 VitroMod.Pult = VitroMod.Pult or {}
 VitroMod.Pult.Urls = {
-	["gm_metro_kalinin_v2"] = 'ws://localhost:8092/',
+	["gm_metro_kalinin_v3"] = 'ws://localhost:8092/',
 	["gm_metro_minsk_1984"] = 'ws://localhost:8090/',
 	["gm_metro_nekrasovskaya_line_v5"] = 'ws://localhost:8081/',
 }
