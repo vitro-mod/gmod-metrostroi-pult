cf972c80317325e7c9e6ecf8e64f44106689c737
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2024-07-29 16:45:39 +0400

    fix nekrs upd v

--- lua/vitro_mod/pult/config.example.lua
+++ lua/vitro_mod/pult/config.example.lua
@@ -3,7 +3,7 @@ VitroMod.Pult = VitroMod.Pult or {}
 VitroMod.Pult.Urls = {
 	["gm_metro_kalinin_v3"] = 'ws://localhost:8092/',
 	["gm_metro_minsk_1984"] = 'ws://localhost:8090/',
-	["gm_metro_nekrasovskaya_line_v5"] = 'ws://localhost:8081/',
+	["gm_metro_nekrasovskaya_line_v6"] = 'ws://localhost:8081/',
 }
 VitroMod.Pult.Name = 'MASTER'
 VitroMod.Pult.Key = 'gm'
