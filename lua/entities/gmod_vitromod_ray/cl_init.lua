0d9e21b21b8b22d9a01645a9d290bf9f129f5fa6
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-07-22 03:57:01 +0200

    rays

--- lua/entities/gmod_vitromod_ray/cl_init.lua
+++ lua/entities/gmod_vitromod_ray/cl_init.lua
@@ -1,44 +1,34 @@
 include('shared.lua')
-
 VitroMod = VitroMod or {}
 VitroMod.Rays = VitroMod.Rays or {}
 VitroMod.RayCap = VitroMod.RayCap or false
-net.Receive("vitromod_ray_data", function(len, ply)
-    VitroMod.Rays = net.ReadTable()
-	for k,v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
+net.Receive('vitromod_ray_data', function(len, ply)
+	VitroMod.Rays = net.ReadTable()
+	for k, v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
 		v:RemoveModels()
 		v.ModelsCreated = false
 	end
 end)
 
-net.Receive("vitromod_ray_cap", function(len, ply)
-    VitroMod.RayCap = net.ReadBool()
-end)
-
-net.Receive("vitromod_ray", function(len, ply)
+net.Receive('vitromod_ray_cap', function(len, ply) VitroMod.RayCap = net.ReadBool() end)
+net.Receive('vitromod_ray', function(len, ply)
 	local rec = net.ReadTable()
-	if VitroMod.Rays[rec.name] ~= nil then
-		VitroMod.Rays[rec.name]['status'] = rec.status
-	end
+	if VitroMod.Rays[rec.name] ~= nil then VitroMod.Rays[rec.name]['status'] = rec.status end
 end)
 
 local function hudName(name, ent)
 	if not ent then return end
 	if not VitroMod.RayCap then return end
-
 	--for name,ent in pairs( VitroMod.Rays ) do
-		local point = ent.pos
-		local data2D = point:ToScreen() -- Gets the position of the entity on your screen
-		if ( not data2D.visible ) then return end
-		draw.SimpleText( name , "DermaLarge", data2D.x, data2D.y, Color( ent.status and 0 or 255, 255, ent.status and 0 or 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
-
+	local point = ent.pos
+	local data2D = point:ToScreen() -- Gets the position of the entity on your screen
+	if not data2D.visible then return end
+	draw.SimpleText(name, 'DermaLarge', data2D.x, data2D.y, Color(ent.status and 0 or 255, 255, ent.status and 0 or 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
 	--end
 end
 
-function ENT:Initialize(arguments)
-    -- print('a')
-end
-
+-- function ENT:Initialize(arguments)
+-- end
 function ENT:Think()
 	self.name = self.name or self:GetNW2String('Name')
 	if not VitroMod.Rays[self.name] then return false end
@@ -49,47 +39,101 @@ function ENT:Think()
 	-- else
 	-- 	self:Play()
 	-- end
-	
-	if self:IsDormant() then 
-		hook.Remove( "HUDPaint", "VitroMod.Rays.Caption"..self:EntIndex())
+	if self:IsDormant() then
+		hook.Remove('HUDPaint', 'VitroMod.Rays.Caption' .. self:EntIndex())
 	else
-		hook.Add( "HUDPaint", "VitroMod.Rays.Caption"..self:EntIndex(), function() hudName(self.name, VitroMod.Rays[self.name]) end)
+		hook.Add('HUDPaint', 'VitroMod.Rays.Caption' .. self:EntIndex(), function() hudName(self.name, VitroMod.Rays[self.name]) end)
 	end
 
-    if not self.ModelsCreated then 
-        self:CreateModels()
-        self.ModelsCreated = true
-    end
+	if not self.ModelsCreated then
+		self:CreateModels()
+		self.ModelsCreated = true
+	end
 end
 
 function ENT:Draw()
-    -- self:DrawModel()
-  end
+	-- self:DrawModel()
+end
 
+ENT.InitialOffset = Vector(0, -78, 67.9 + 3)
 ENT.Models = {
-    ["Lamp"] = {["Model"] = "models/mn_signs/light_sensor_emitter.mdl", ["Offset"] = Vector(0,80,40), ["Angle"] = Angle(0,90,0)},
-    ["Sensor"] = {["Model"] = "models/mn_signs/light_sensor.mdl", ["Offset"] = Vector(94,-15,-30), ["Angle"] = Angle(0,-90,0)},
-    ["Light"] = {["Model"] = "models/mus/direction_lamp_w.mdl", ["Offset"] = Vector(0,80-1.7,40+2), ["Angle"] = Angle(0,-90,0)},
+	['Lamp'] = {
+		['Model'] = 'models/mn_signs/light_sensor_emitter.mdl',
+		['Offset'] = Vector(0, 80, 40),
+		['Angle'] = Angle(0, 90, 0)
+	},
+	['Sensor'] = {
+		['Model'] = 'models/mn_signs/light_sensor.mdl',
+		['Offset'] = Vector(-94, -15, -30),
+		['Angle'] = Angle(0, -90, 0)
+	},
+	['Light'] = {
+		['Model'] = 'models/mus/direction_lamp_w.mdl',
+		['Offset'] = Vector(0, -1.7, 2),
+		['Angle'] = Angle(0, -90, 0)
+	},
 }
 
 function ENT:CreateModels()
-    self.ClientSideModels = {}
-    for k,v in pairs(self.Models) do
-        self.ClientSideModels[k] = ClientsideModel(v.Model)
-		local pos = Vector(v.Offset:Unpack())
-		pos:Rotate(self:GetAngles())
-        self.ClientSideModels[k]:SetPos(self:GetPos() + pos)
-        self.ClientSideModels[k]:SetAngles(self:GetAngles() + v.Angle)
-        self.ClientSideModels[k]:Spawn()
-    end
+	local sensorOffset = self.Models['Sensor'].Offset + Vector(0, -(VitroMod.Rays[self.name].options.sensorXOffset or 0), VitroMod.Rays[self.name].options.sensorZOffset or 0)
+	-- local sensorOffset = self.Models['Sensor'].Offset
+	local lampOffset = self.Models['Lamp'].Offset + Vector(0, -(VitroMod.Rays[self.name].options.lampXOffset or 0), VitroMod.Rays[self.name].options.lampZOffset or 0)
+	-- local lampOffset = self.Models['Lamp'].Offset
+	local direction = (self.InitialOffset + sensorOffset - lampOffset):GetNormalized()
+	local directionAngle = direction:Angle()
+	-- print('Direction:', direction)
+	-- print('Direction Angle:', directionAngle)
+	self.ClientSideModels = {}
+	for k, v in pairs(self.Models) do
+		self.ClientSideModels[k] = ClientsideModel(v.Model)
+		self.ClientSideModels[k]:SetParent(self)
+		self.ClientSideModels[k]:Spawn()
+		local offset = v.Offset or Vector(0, 0, 0)
+		local angle = v.Angle or Angle(0, 0, 0)
+		if k == 'Sensor' then
+			offset = sensorOffset
+			self.ClientSideModels[k]:AddCallback('BuildBonePositions', function(ent, numbones)
+				local mat = ent:GetBoneMatrix(1)
+				if not mat then return end
+				-- x - across track
+				-- y - vertical
+				-- z - along track
+				mat:SetAngles(self:LocalToWorldAngles(Angle(0, 90 + directionAngle[2], 90 + directionAngle[1])))
+				ent:SetBoneMatrix(1, mat)
+			end)
+		elseif k == 'Lamp' then
+			offset = lampOffset
+			angle = Angle(-directionAngle[1], 180 + directionAngle[2], 0)
+		elseif k == 'Light' then
+			if not IsValid(self.PT) then self.PT = ProjectedTexture() end
+			self.PT:SetPos(self.ClientSideModels['Lamp']:GetPos())
+			local lmpang = self.ClientSideModels['Lamp']:GetAngles()
+			self.PT:SetAngles(Angle(lmpang[1] + 180, lmpang[2], lmpang[3]))
+			self.PT:SetEnableShadows(true)
+			self.PT:SetTexture('effects/flashlight001')
+			self.PT:SetColor(Color(255, 255, 255))
+			self.PT:SetFarZ(300)
+			self.PT:SetFOV(3)
+			self.PT:SetBrightness(5)
+			self.PT:Update()
+		end
+
+		local position = self:LocalToWorld(offset)
+		local angles = self:LocalToWorldAngles(angle)
+		self.ClientSideModels[k]:SetPos(position)
+		self.ClientSideModels[k]:SetAngles(angles)
+	end
 end
 
 function ENT:RemoveModels()
-    for k,v in pairs(self.ClientSideModels) do
-        SafeRemoveEntity(self.ClientSideModels[k])
-        self.ClientSideModels[k] = nil
-    end
-    self.ClientSideModels = {}
+	if IsValid(self.PT) then self.PT:Remove() end
+	if not self.ClientSideModels then return end
+	for k, v in pairs(self.ClientSideModels) do
+		SafeRemoveEntity(self.ClientSideModels[k])
+		self.ClientSideModels[k] = nil
+	end
+
+	self.ClientSideModels = {}
 end
 
 function ENT:OnRemove()
--- lua/entities/gmod_vitromod_ray/init.lua
+++ lua/entities/gmod_vitromod_ray/init.lua
@@ -1,129 +1,128 @@
-AddCSLuaFile( "cl_init.lua" )
-AddCSLuaFile( "shared.lua" )
-
+AddCSLuaFile('cl_init.lua')
+AddCSLuaFile('shared.lua')
 include('shared.lua')
-
 VitroMod = VitroMod or {}
 VitroMod.Rays = VitroMod.Rays or {}
 VitroMod.Rays.Name = VitroMod.Rays.Name or 'ray1'
 VitroMod.Rays.Status = VitroMod.Rays.Status or {}
 VitroMod.Rays.Caption = false
-
 hook.Add('VitroMod.Rays.Status', 'VitroMod.Rays.Status.set', function(name, status)
 	VitroMod.Rays.Status[name] = status
 	VitroMod.Rays.sendRay(name)
 end)
 
 function ENT:Initialize()
-	self:SetModel( "models/mn_signs/light_sensor.mdl" )
-	self:SetNW2String('Name',self:GetName())
+	self:SetModel('models/mn_signs/light_sensor.mdl')
+	self:SetNW2String('Name', self:GetName())
 end
-
 
 function ENT:Think()
-
 end
 
-VitroMod.Rays.atLook = function(trace, ply, name, userOffset, userAngle)
+VitroMod.Rays.atLook = function(trace, ply, name, options )
 	local needSpawn = true
-	for k,v in pairs(ents.FindInSphere(trace.HitPos, 100)) do 
+	for k, v in pairs(ents.FindInSphere(trace.HitPos, 100)) do
 		if v:GetClass() == 'gmod_vitromod_ray' and v:EntIndex() == VitroMod.Rays.LastRay then needSpawn = false end
 	end
-	local ray = needSpawn and ents.Create("gmod_vitromod_ray") or Entity(VitroMod.Rays.LastRay)
+
+	local ray = needSpawn and ents.Create('gmod_vitromod_ray') or Entity(VitroMod.Rays.LastRay)
 	local hitang = trace.HitNormal:Angle()
-	local angles = Angle( hitang[1]+90, hitang[2], hitang[3])
-	local offset = Vector(0,0,2.1)
+	local angles = Angle(hitang[1] + 90, hitang[2], hitang[3])
+	local offset = Vector(0, 0, 2.1)
 	offset:Rotate(angles)
-    local pos = trace.HitPos + offset + userOffset
-    local ang = angles + userAngle
-    local tr = Metrostroi.RerailGetTrackData(trace.HitPos,ply:GetAimVector())
-    if tr then pos = tr.centerpos + offset + userOffset end
+	local pos = trace.HitPos
+	local ang = angles
+	local tr = Metrostroi.RerailGetTrackData(trace.HitPos, ply:GetAimVector())
+	if tr then pos = tr.centerpos + offset end
 	if tr then ang = tr.forward:Angle() end
-	ray:SetPos( pos )
-	ray:SetAngles( ang )
+	ray:SetPos(pos)
+	ray:SetAngles(ang)
+	ray.options = options or {}
 	local nm = VitroMod.Rays.Name
 	if name then nm = name end
 	if needSpawn then
 		ray:SetName(nm)
-		ray:SetNW2String('Name',nm)
+		ray:SetNW2String('Name', nm)
 		ray:Spawn()
 		VitroMod.Rays.LastRay = ray:EntIndex()
-		undo.Create("Ray")
-			undo.AddEntity(ray)
-			undo.SetPlayer(ply)
+		undo.Create('Ray')
+		undo.AddEntity(ray)
+		undo.SetPlayer(ply)
 		undo.Finish()
 	end
+
 	VitroMod.Rays.send()
 end
 
 VitroMod.Rays.remove = function(trace)
-	for k,v in pairs(ents.FindInSphere(trace.HitPos, 100)) do 
+	for k, v in pairs(ents.FindInSphere(trace.HitPos, 100)) do
 		if v:GetClass() == 'gmod_vitromod_ray' then v:Remove() end
 	end
 end
 
 VitroMod.Rays.flush = function()
-	for k,v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
+	for k, v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
 		SafeRemoveEntity(v)
 	end
 end
 
 VitroMod.Rays.send = function(ply)
-	if not VitroMod.RayNWAdded then 
-		util.AddNetworkString("vitromod_ray_data") 
-		VitroMod.RayNWAdded = true 
+	if not VitroMod.RayNWAdded then
+		util.AddNetworkString('vitromod_ray_data')
+		VitroMod.RayNWAdded = true
 	end
+
 	local clientData = {}
-	for k,v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
+	for k, v in pairs(ents.FindByClass('gmod_vitromod_ray')) do
 		local name = v:GetName()
 		clientData[name] = {}
 		clientData[name]['pos'] = v:GetPos()
 		clientData[name]['status'] = VitroMod.Rays.Status[name] or false
+		clientData[name]['options'] = v.options or {}
 	end
-	net.Start("vitromod_ray_data")
+
+	net.Start('vitromod_ray_data')
 	net.WriteTable(clientData)
 	if not ply then
 		net.Broadcast()
-	else 
-		net.Send(ply) 
-	end	
+	else
+		net.Send(ply)
+	end
 end
-
 
 VitroMod.Rays.sendRay = function(name, ply, mute)
 	--if ply then print(ent:GetName(), ply:GetName(), mute) end
-	if not VitroMod.RayNWAdded3 then 
-		util.AddNetworkString("vitromod_ray") 
-		VitroMod.RayNWAdded3 = true 
+	if not VitroMod.RayNWAdded3 then
+		util.AddNetworkString('vitromod_ray')
+		VitroMod.RayNWAdded3 = true
 	end
+
 	local clientData = {}
 	clientData['name'] = name
 	clientData['status'] = mute and false or VitroMod.Rays.Status[name]
-	net.Start("vitromod_ray")
+	net.Start('vitromod_ray')
 	net.WriteTable(clientData)
 	if not ply then
 		net.Broadcast()
-	else 
-		net.Send(ply) 
-	end	
+	else
+		net.Send(ply)
+	end
 end
 
 VitroMod.Rays.sendCap = function(ply)
-	if not VitroMod.RayNWAdded2 then 
-		util.AddNetworkString("vitromod_ray_cap") 
-		VitroMod.RayNWAdded2 = true 
+	if not VitroMod.RayNWAdded2 then
+		util.AddNetworkString('vitromod_ray_cap')
+		VitroMod.RayNWAdded2 = true
 	end
-	net.Start("vitromod_ray_cap")
+
+	net.Start('vitromod_ray_cap')
 	net.WriteBool(VitroMod.Rays.Caption)
 	net.Broadcast()
 	if not ply then return end
 	net.Send(ply)
 end
 
-hook.Add("PlayerSpawn", 'VitroMod.Rays', function(ply)
-	VitroMod.Rays.send(ply)
-end)
-
+hook.Add('PlayerSpawn', 'VitroMod.Rays', function(ply) VitroMod.Rays.send(ply) end)
 function ENT:OnRemove()
 	VitroMod.Rays.send(ply)
 end
