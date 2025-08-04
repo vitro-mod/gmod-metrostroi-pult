75130c9af545d8e43cc5d1ce2f58bc466424b969
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-08-04 23:32:11 +0200

    GermoGate improve

--- lua/GermoGates.lua
+++ lua/GermoGates.lua
@@ -1,14 +1,20 @@
 VitroMod.Pult.GermoGates = {
     Entities = {},
+    Inverted = {},
     States = {},
     Locked = {},
-    Init = function(entities)
+    Init = function(entities, invert)
+        local invertAll = invert or false
         if not entities then return false end
         for k, entity in pairs(entities) do
             if not IsValid(entity) then return false end
 
             VitroMod.Pult.GermoGates.Entities[entity:GetName()] = entity
-            VitroMod.Pult.GermoGates.States[entity:GetName()] = entity:GetSaveTable().m_toggle_state
+            VitroMod.Pult.GermoGates.Inverted[entity:GetName()] = invertAll
+
+            local state = entity:GetSaveTable().m_toggle_state
+
+            VitroMod.Pult.GermoGates.States[entity:GetName()] = invertAll and VitroMod.Pult.GermoGates.InvertControl(state) or state
             VitroMod.Pult.GermoGates.Locked[entity:GetName()] = false
 
             addEntityOutputHook(entity, 'OnFullyOpen', 'mkSend')
@@ -19,7 +25,9 @@ VitroMod.Pult.GermoGates = {
     end,
     UpdateState = function(entity)
         if not IsValid(entity) then return false end
-        VitroMod.Pult.GermoGates.States[entity:GetName()] = entity:GetSaveTable().m_toggle_state
+        local state = entity:GetSaveTable().m_toggle_state
+        local inverted = VitroMod.Pult.GermoGates.Inverted[entity:GetName()] or false
+        VitroMod.Pult.GermoGates.States[entity:GetName()] = inverted and VitroMod.Pult.GermoGates.InvertControl(state) or state
     end,
     UpdateLocks = function(lockStates)
         for name, lockState in pairs(lockStates) do
@@ -31,10 +39,27 @@ VitroMod.Pult.GermoGates = {
     GetAll = function()
         return VitroMod.Pult.GermoGates.States
     end,
+    Switch = function(name, to)
+        local inverted = VitroMod.Pult.GermoGates.Inverted[name] or false
+        if inverted then to = not to end
+        name = name:Replace('.','_')
+        local ent = VitroMod.Pult.GermoGates.Entities[name]
+        if not IsValid(ent) then return end
+        ent:Fire('Unlock')
+        ent:Fire(not to and 'Close' or 'Open')
+    end,
+    InvertControl = function(ctrl)
+        if ctrl == 1 then return 0 end
+        if ctrl == 0 then return 1 end
+        return ctrl
+    end,
 }
 
 function sendMKInfoV2()
     local ctrl = CALLER:GetSaveTable().m_toggle_state
+    if VitroMod.Pult.GermoGates.Inverted[CALLER:GetName()] then
+        ctrl = VitroMod.Pult.GermoGates.InvertControl(ctrl)
+    end
     local name = CALLER:GetName()
     local mkmsg = 'BV_' .. util.TableToJSON({
         [name] = ctrl
