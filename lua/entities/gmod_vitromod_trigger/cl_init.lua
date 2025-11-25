1d916060a0f2134146bd02720fa826629dfd399f
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-11-25 21:10:45 +0100

    triggers signal helper

--- lua/entities/gmod_vitromod_trigger/cl_init.lua
+++ lua/entities/gmod_vitromod_trigger/cl_init.lua
@@ -18,6 +18,10 @@ net.Receive("vitromod_trigger_draw", function(len, ply)
 	VitroMod.draw = net.ReadBool()
 	--print('draw',VitroMod.draw)
 end)
+local freeColor = Color(255, 200, 0)
+local occColor = Color(255, 0, 0)
+local dopColor = Color(255, 255, 255)
+local dop_length = 100
 local function drawBox()
     if not VitroMod.draw then return end
     if not VitroMod.Triggers then return end
@@ -26,8 +30,11 @@ local function drawBox()
 		for k2,v2 in pairs(v) do
 			if k2 ~= "occ" then
 				--local col = v2.pos:DistToSqr(Entity(1):GetEyeTrace().HitPos) > 100 and Color(255,200,0) or Color(0,255,0)
-				local col = v.occ and Color(255,0,0) or Color(255,200,0)
-				if v2.pos and v2.angle and v2.min and v2.max then render.DrawWireframeBox(v2.pos,v2.angle,v2.min,v2.max, col) end
+				local col = v.occ and occColor or freeColor
+				if v2.pos and v2.angle and v2.min and v2.max then
+					render.DrawWireframeBox(v2.pos,v2.angle,v2.min,v2.max, col)
+					render.DrawWireframeBox(v2.pos, v2.angle, v2.min + Vector(20, -dop_length, 0), v2.max + Vector(-20, dop_length, 0), dopColor)
+				end
 				if v2.beg then render.DrawSphere(v2.beg, 5, 5, 5, col) end
 				if v2.pos then render.DrawSphere(v2.pos, 10, 5, 5, col) end
 				if v2.fin then render.DrawWireframeSphere(v2.fin, 5, 5, 5, col) end
