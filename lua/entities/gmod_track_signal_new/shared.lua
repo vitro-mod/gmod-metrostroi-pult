4c32a84d501998391d498c2c1254e600be47643a
Victor Troitskii <108282310+vitro-mod@users.noreply.github.com>
2025-12-06 00:35:18 +0100

    fix left arrow pointer offset

--- lua/entities/gmod_track_signal_new/shared.lua
+++ lua/entities/gmod_track_signal_new/shared.lua
@@ -385,7 +385,7 @@ for i = 0, (#ENT.TrafficLightModels) do
 		Vector(8),    -- Sep (on short kron) Indicator model offset
 		Vector(-12, 0, 0), -- Sep (on short kron) Indicator model offset if left
 		Vector(3, 0, 3), -- Arrow offset
-		Vector(20, 0, -12), -- Arrow offset if left
+		Vector(0, 0, -12), -- Arrow offset if left
 	}
 	ENT.TrafficLightModels[i].LampBase = { model = "models/metrostroi/signals/mus/lamp_base.mdl" }
 	ENT.TrafficLightModels[i].SignLetterSmall = {
